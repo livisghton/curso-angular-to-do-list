2e970218feca9dbfc4e64464ed95d2f34874ab6b
Livisghton Araujo <laraujo@nelogica.com.br>
2023-06-05 17:00:02 -0300

    Adiciona informações na localStorage

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -10,12 +10,12 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || "[]");
   
   constructor() {}
 
   ngDoCheck(): void {
-    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();
   }
 
   /**
@@ -61,6 +61,14 @@ export class TodoListComponent implements DoCheck {
       if (confirm) {
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage() {
+
+    if(this.taskList) {
+      this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 
